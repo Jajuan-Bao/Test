1e398ea641a867345043cbdcfacdd2b483b5761d
Jajuan.Bao <24786001+Jajuan-Bao@users.noreply.github.com>
2017-10-01 13:59:51 +0800

    Create Random.h

---
+++ Random.h
@@ -0,0 +1,10 @@
+//
+// Created by 516370910112 on 2016/11/30 0030.
+//
+
+#ifndef RANDOM_RANDOM_H
+#define RANDOM_RANDOM_H
+void RandomInit();
+int RandomInterger(int low, int high);
+double RandomDouble(double low, double high);
+#endif //RANDOM_RANDOM_H
